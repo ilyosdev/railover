fb5b24b0d58041a1ab6190d17a862856da2ab98a
Ilyos Olimov <ilyosolimoff@gmail.com>
2026-01-12 20:31:27 +0500

    fix: correct static file path for frontend serving

--- src/app.ts
+++ src/app.ts
@@ -99,7 +99,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static(path.join(__dirname, '../dist-frontend')))
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use(CaptainConstants.healthCheckEndPoint, function (req, res, next) {
     res.send(CaptainManager.get().getHealthCheckUuid())
